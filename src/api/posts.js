a0354f1eb3b19aa38f7244c6fca03aab69680144
MarianaPertuz17 <marianapertuzv@gmail.com>
2023-02-15 19:12:17 -0500

    feat: mutation adding post

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,8 +1,13 @@
-export function createPost({ title, body }) {
+import axios from "axios";
+
+export function createPost(post) {
+  const { title, description } = post
   return axios
-    .post("http://localhost:3000/posts", {
+    .post("http://localhost:3000/posts",{
       title,
-      body
+      description,
+      id: Date.now()
     })
-    .then(res => res.data)
+    .then(res => console.log(res, 'lla res'))
+    .catch(e => console.log(e, 'error'))
 }
--- src/screens/posts/index.jsx
+++ src/screens/posts/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useMutation, useQuery } from "react-query"
 import { getCharactersPaginated } from "../../api/characters"
+import { createPost } from "../../api/posts"
 import { CharacterCard } from "../../components/characterCard"
 import styles from './styles.module.css'
 
@@ -11,24 +12,35 @@ const initialFormData = {
 
 export function Posts () {
   const [ formData, setFormData ] = useState(initialFormData);
+
+  const createPostMutation = useMutation({
+    mutationFn: createPost,
+    onSuccess: () => {
+      console.log('Ready')
+    },
+    onError: () => console.log('error')
+  })
   
   const handleSubmit = (e) => {
     e.preventDefault()
+    createPostMutation.mutate(formData)
+    setFormData(initialFormData)
   }
 
   const handleChange = (e) => {
-    const { value, name, type } = e.target;
-    if (type === 'radio') setSelectedUser(value);
-    else setFormData(prevState => ({...prevState, [name]: value}));
+    const { value, name } = e.target;
+    setFormData(prevState => ({...prevState, [name]: value}));
   }
 
-  const createPostMutation = useMutation()
-
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <input placeholder='Title' name='title' value={formData.title} type='text' onChange={handleChange}/>
-      <input placeholder='Password' name='password' value={formData.password} type='password' onChange={handleChange}/>
-      <button type='submit'>Log in</button>
-    </form>  
+    <div>
+      {createPostMutation.isError && JSON.stringify(createPostMutation.error)}
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input placeholder='Title' name='title' value={formData.title} type='text' onChange={handleChange}/>
+        <input placeholder='Description' name='description' value={formData.description} type='description' onChange={handleChange}/>
+        <button type='submit' disabled={createPostMutation.isLoading}>Create Post</button>
+      </form> 
+    </div>
+     
   )
 }
